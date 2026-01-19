71f66a6afb79e39232ac9888845fd0d15011ca74
Maria Cláudia <mariaclaudiapio@gmail.com>
2026-01-19 16:34:05 -0300

    começando estudos de structs

--- programa.c
+++ programa.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
+// Definição de uma estrutura "Aluno"
+    struct Aluno
+    {
+        char nome[50];
+        int idade;
+        float media;
+    };
+
+    // uso do typedef para criação de um tipo "Aluno"
+    typedef struct 
+    {
+        char nome[50];
+        int idade;
+        float media;
+    } TipoAluno;
+
+
 int main()
 {
     printf("Hello, World!\n");
+
+    // Criando e inicializando uma variável do tipo Aluno
+    TipoAluno aluno1 = {"João", 20, 8.5};
+
+    // Acessando os dados com o operador "."
+    printf("Aluno: %s\n", aluno1.nome); 
+    printf("Média: %.2f\n", aluno1.media);        
+
+    
+    
 }
